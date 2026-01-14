a3b5afd5aefb38cc4c7a0b74e0fbca2b73eb750a
Humza920 <humza.rehman.779@gmail.com>
2026-01-14 12:07:16 +0500

    PUSHING IT

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://c-m-s-backend.vercel.app/",
     withCredentials: true
 })
